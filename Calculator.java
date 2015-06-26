28fbc0126c08d4c05f9c215fcf2dd70b76f6fec7
Anvar Alekperov <a.alakbarov@gmail.com>
2015-06-26 15:55:38 +0300

    update calc

--- Calculator.java
+++ Calculator.java
@@ -1,19 +1,41 @@
-package GHHTP1;
+
 
 public class Calculator {
-    private int result;
-
-    public void add(int... params){
-        for(Integer param: params){
-            this.result += param;
-        }
+    private int summ;
+    private int minus;
+	private double result;
+	
+    public double addSumm(double... params){
+	result = params[0] + params[1];
+	return result;
+    }
+	
+	    public double addMinus(double... params){
+	result = params[0] - params[1];
+	return result;
+    }
+	
+		    public double addDecart(double... params){
+	result = params[0] * params[1];
+	return result;
+    }
+	
+			    public double addDevision(double... params){
+	result = params[0] / params[1];
+	return result;
+    }
+	
+			    public double addPow(double... params){							
+	result = Math.pow(params[0], params[1]);
+	return result;
     }
 
-    public int getResult() {
+	    public double getResult() {
         return this.result;
     }
-
+	
     public void clearResult(){
        this.result = 0;
+
     }
 }
--- InteractRunner.java
+++ InteractRunner.java
@@ -1,4 +1,4 @@
-package GHHTP1;
+
 
 import java.util.Scanner;
 
@@ -11,14 +11,49 @@ public class InteractRunner {
 
         Calculator calc = new Calculator();
         String exit = "no";
+		String clear = "no";
+		String choose = "0";
         while(!exit.equals("yes")) {
             System.out.println("Enter your first arg");
             String first = scanner.next();
             System.out.println("Enter your second arg");
             String second = scanner.next();
-            calc.add(Integer.valueOf(first), Integer.valueOf(second));
-            System.out.println("results: " + calc.getResult());
-            calc.clearResult();
+			System.out.println("What do you want to do with this");
+			System.out.println("1 - summ ");
+			System.out.println("2 - minus ");
+			System.out.println("3 - decart ");
+			System.out.println("4 - devision ");
+			System.out.println("5 - pow ");
+			choose = scanner.next();
+						
+									if(choose.equals("1")){
+            calc.addSumm(Integer.valueOf(first), Integer.valueOf(second));
+			}
+			
+							if(choose.equals("2")){
+            calc.addMinus(Integer.valueOf(first), Integer.valueOf(second));
+			}
+			
+										if(choose.equals("3")){
+            calc.addDecart(Integer.valueOf(first), Integer.valueOf(second));
+			}
+			
+										if(choose.equals("4")){
+            calc.addDevision(Integer.valueOf(first), Integer.valueOf(second));
+			}
+			
+													if(choose.equals("5")){
+            calc.addPow(Integer.valueOf(first), Integer.valueOf(second));
+			}
+			
+			System.out.println("results: " + calc.getResult());
+
+			System.out.println("Clear result yes/no ?");
+			clear = scanner.next();
+			if(clear.equals("yes")){
+			calc.clearResult();	
+			System.out.println("Result is cleaned");
+			}
             System.out.println("Exit yes/no ?");
             exit = scanner.next();
         }
