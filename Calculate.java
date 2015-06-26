e1ed5163c64086c551b51e5a3c978120b6ee5b17
Anvar Alekperov <a.alakbarov@gmail.com>
2015-06-26 14:27:58 +0300

    Calculate create

--- Calculate.java
+++ Calculate.java
@@ -1,5 +1,20 @@
 public class Calculate{
 	public static void main(String[] arg){
 		System.out.println("Hello");
+		int first = Integer.valueOf(arg[0]);
+		int second = Integer.valueOf(arg[1]);
+		int summ = first + second;
+		int minus = first - second;
+		int decart = first * second;
+		int devision = first / second;
+		int pow = (int)Math.pow(first, second);
+		
+		System.out.println(summ);
+		System.out.println(minus);
+		System.out.println(decart);
+		System.out.println(devision);
+		System.out.println(pow);
+		
+		
 	}
 }
